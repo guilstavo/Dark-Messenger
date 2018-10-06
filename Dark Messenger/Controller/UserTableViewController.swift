b56d6a264cf2b9074c5335c0273026e7c196db5f
guilstavo <gustavomartinis@gmail.com>
2018-10-06 16:59:18 +0100

    Add alert to confirm Logout

--- Dark Messenger/Controller/UserTableViewController.swift
+++ Dark Messenger/Controller/UserTableViewController.swift
@@ -117,30 +117,36 @@ class UserTableViewController: UITableViewController {
             textField = alertTextField
         }
         alert.addAction(action)
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
-            print("Cancel button pressed")
-        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         present(alert, animated: true, completion: nil)
     }
 
     @IBAction func logOutPressed(_ sender: Any) {
-        do {
-            try Auth.auth().signOut()
-            
-            let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-            if let viewController = mainStoryboard.instantiateViewController(withIdentifier: "WelcomeViewController") as? WelcomeViewController {
+        
+        let logOutAlert = UIAlertController(title: "Are you sure you want to log out?", message: nil, preferredStyle: .actionSheet)
+        let logOutAction = UIAlertAction(title: "Log Out", style: .destructive) { (action) in
+            do {
+                try Auth.auth().signOut()
                 
-                let navVC = mainStoryboard.instantiateViewController(withIdentifier: "navigationController") as! UINavigationController
-                
-                // set the "root" VC of the NavVC to your SearchResultsTableViewController
-                navVC.setViewControllers([viewController], animated: false)
-                
-                // use the new NavVC as the new rootViewController
-                UIApplication.shared.keyWindow?.rootViewController = navVC
-                UIApplication.shared.keyWindow?.rootViewController?.navigationController?.popToRootViewController(animated: true)
+                let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                if let viewController = mainStoryboard.instantiateViewController(withIdentifier: "WelcomeViewController") as? WelcomeViewController {
+                    
+                    let navVC = mainStoryboard.instantiateViewController(withIdentifier: "navigationController") as! UINavigationController
+                    
+                    // set the "root" VC of the NavVC to your SearchResultsTableViewController
+                    navVC.setViewControllers([viewController], animated: false)
+                    
+                    // use the new NavVC as the new rootViewController
+                    UIApplication.shared.keyWindow?.rootViewController = navVC
+                    UIApplication.shared.keyWindow?.rootViewController?.navigationController?.popToRootViewController(animated: true)
+                }
+            } catch {
+                print("error, thare was a problem signing out")
             }
-        } catch {
-            print("error, thare was a problem signing out")
         }
+        
+        logOutAlert.addAction(logOutAction)
+        logOutAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(logOutAlert, animated: true, completion: nil)
     }
 }
